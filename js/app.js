a08026efe9d095a944e65d102e87ed0e0faf3fe5
David Goss <dgoss@whiteclarkegroup.com>
2016-01-07 08:53:25 +0000

    Splitting generator out into own module, using RequireJS

--- js/app.js
+++ js/app.js
@@ -1,36 +1,37 @@
-angular.module("rwdRandomiser", [])
-	// using custom interpolation symbols to avoid conflicts with jekyll
-	.config(["$interpolateProvider", function($interpolateProvider) {
-		$interpolateProvider.startSymbol("##");
-		$interpolateProvider.endSymbol("##");
-	}])
-	.controller("AppController", ["$scope", function($scope) {
-		function randomInt(min, max) {
-			return Math.floor(Math.random() * (max - min + 1) + min);
-		}
+require(["generator"], function(generator) {
+    angular.module("rwdRandomiser", [])
+        // using custom interpolation symbols to avoid conflicts with jekyll
+        .config(["$interpolateProvider", function($interpolateProvider) {
+            $interpolateProvider.startSymbol("##");
+            $interpolateProvider.endSymbol("##");
+        }])
+        .controller("AppController", ["$scope", function($scope) {
+            function randomise() {
+                var maxWidth = window.innerWidth - 50,
+                    maxHeight = window.innerHeight - 200;
 
-		function randomise() {
-			var maxWidth = window.innerWidth - 50,
-				maxHeight = window.innerHeight - 200;
+                $scope.model.width = generator(maxWidth / 4, maxWidth);
+                $scope.model.height = generator(maxHeight / 2, maxHeight);
 
-            $scope.model.width = randomInt(maxWidth / 4, maxWidth);
-            $scope.model.height = randomInt(maxHeight / 2, maxHeight);
+                $scope.iframe
+                    .attr("width", $scope.model.width)
+                    .attr("height", $scope.model.height)
+                    .attr("src", $scope.model.url);
+            }
 
-			$scope.iframe
-				.attr("width", $scope.model.width)
-				.attr("height", $scope.model.height)
-				.attr("src", $scope.model.url);
-		}
+            angular.extend($scope, {
+                model: {
+                    url: "http://davidgoss.co/",
+                    width: 320,
+                    height: 480
+                },
+                iframe: angular.element(document).find("iframe"),
+                randomise: randomise
+            });
 
-		angular.extend($scope, {
-            model: {
-                url: "http://davidgoss.co/",
-                width: 320,
-                height: 480
-            },
-			iframe: angular.element(document).find("iframe"),
-			randomise: randomise
-		});
-
-        randomise();
-	}]);
+            randomise();
+        }]);
+        angular.element(document).ready(function() {
+            angular.bootstrap(document, ["rwdRandomiser"]);
+        });
+});
